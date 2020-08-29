622288c7f0d600d5324ee5208e0814450c6a6541
smshack <5432tat@naver.com>
2020-08-30 02:51:14 +0900

    # WARNING: head commit changed in the meantime
    Servlet 시작
    특징
    - 동적 웹 어플리케이션 컴포넌트
    - .java 확장자
    - 클라이언트의 요청에 동적으로 작동하고, 응답은 html을 이용
    - MVC패턴에서 Controller로 이용됨
    
    
    프로젝트 생성
    다이나믹 웹 프로젝트
    
    서블릿 생성
    
    
    web.xml
    서블릿 맵핑
    너무 길고, 보안에 노출되어 있는 경로를 단단하게 맵핑하는 것
    
    기존 경로
    http://localhost:8080/hellowrold/servlet/lecture3.ex.HelloWrold
    
    URL 매핑 경로
    http://localhost:8080/hellowrold/HelloWorld
    
    매핑을 하는 2가지 방법
    web.xml을 이용하는 방법
    <servlet-name>
    임의의 이름을 만들어 줌
    
    <servlet-class>
    매핑할 클래스 파일명을 패키지명을 포함하여 정확하게 입력
    
    <url-pattern>
    servlet-class의 클래스를 매핑할 임의의 이름을 입력
    주의할 점은 / 로 시작해야 함
    
    
    
    서블릿에 직접 적어주는 방법
    이방법이 편하고 대체로 쓰이는 방식
    어노테이션 이용
    @WebServlet(description = "자바 시험 프로젝트", urlPatterns = { "/HelloWorld" })

---
+++ src/lecture3/ex/HelloWorld.java
@@ -0,0 +1,41 @@
+package lecture3.ex;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+@WebServlet(description = "자바 시험 프로젝트", urlPatterns = { "/HelloWorld" }) 
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public HelloWorld() {
+        // TODO Auto-generated constructor stub
+    	super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWorld!!! doGet");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
